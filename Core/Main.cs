4f5ffb9fae4d8dbdf4639e0478c265ba469ed6e8
dfcut8 <dfcut8@proton.me>
2026-07-13 09:35:14 -0400

    creating a simple tilemap to play around with

---
+++ Core/Main.cs
@@ -0,0 +1,10 @@
+using Godot;
+
+namespace RealTimeStrategy.Core;
+
+public partial class Main : Node2D
+{
+    public override void _Ready() { }
+
+    public override void _Process(double delta) { }
+}
