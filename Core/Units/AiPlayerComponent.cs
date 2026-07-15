c4c3c1db28595dc1216a2b1ba4a1b361d88adb35
dfcut8 <dfcut8@proton.me>
2026-07-15 10:46:56 -0400

    scanning for enemies

--- Core/Units/AiPlayerComponent.cs
+++ Core/Units/AiPlayerComponent.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using Godot;
 
 namespace RealTimeStrategyTutorial.Core.Units;
@@ -10,10 +11,38 @@ public partial class AiPlayerComponent : Node
     [Export]
     public float DetectRate { get; set; }
 
-    private float lastDetectTime;
-    private Unit[] enemyList;
+    private double lastDetectTime;
+    private List<Unit> enemyList = [];
+    private Unit owner = new();
 
-    public override void _Ready() { }
+    public override void _Ready()
+    {
+        owner = GetParent<Unit>();
+    }
 
-    public override void _Process(double delta) { }
+    public override void _Process(double delta)
+    {
+        var time = Time.GetUnixTimeFromSystem();
+        if (time - lastDetectTime < DetectRange)
+        {
+            lastDetectTime = time;
+            UpdateEnemyList();
+            Detect();
+        }
+    }
+
+    private void Detect() { }
+
+    private void UpdateEnemyList()
+    {
+        enemyList.Clear();
+        var listFromGroup = GetTree().GetNodesInGroup("UnitsPlayer");
+        foreach (var obj in listFromGroup)
+        {
+            if (obj is Unit u)
+            {
+                enemyList.Add(u);
+            }
+        }
+    }
 }
