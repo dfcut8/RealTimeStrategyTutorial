a7832d31d34c8a499d73023b91739a638500a5b8
dfcut8 <dfcut8@proton.me>
2026-07-16 17:32:32 -0400

    done with codex

--- Core/Camera2d.cs
+++ Core/Camera2d.cs
@@ -2,21 +2,18 @@ using Godot;
 
 public partial class Camera2d : Camera2D
 {
-    public override void _Ready() { }
-
-    public override void _Process(double delta) { }
-
     public override void _UnhandledInput(InputEvent e)
     {
-        if (e is InputEventMouseButton mouseButtonEvent)
+        if (
+            e is not InputEventMouseMotion mouseMotionEvent
+            || (mouseMotionEvent.ButtonMask & MouseButtonMask.Middle) == 0
+        )
         {
-            if (mouseButtonEvent.ButtonMask == MouseButtonMask.Middle)
-            {
-                GD.Print($"Setting camera position to: {mouseButtonEvent.GlobalPosition}");
-                var rect = GetViewport().GetVisibleRect();
-                GD.Print($"rect={rect.Size}");
-                Position = mouseButtonEvent.GlobalPosition - (rect.Size / 2);
-            }
+            return;
         }
+
+        Position -= mouseMotionEvent.Relative / Zoom;
+        ResetSmoothing();
+        GetViewport().SetInputAsHandled();
     }
 }
